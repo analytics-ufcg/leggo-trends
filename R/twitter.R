c1bc68590e84dbd1534df0937c83fc40c8b03e80
Matheus Leal <matheus.leal@ccc.ufcg.edu.br>
2019-08-07 11:19:38 -0300

    Baixando todos os tweets estourando o limite maximo

--- R/twitter.R
+++ R/twitter.R
@@ -2,17 +2,7 @@ library(rtweet)
 library(lubridate)
 library(tidyverse)
 
-get_tweets_pls <- function(words_df) {
-  tweets <- purrr::map_df(words_df$termos, ~ .get_tweets(.x))
-  
-}
-
-.get_tweets <- function(word) {
-  print(paste0("Baixando tweets com termo: '", word, "'..."))
-  tweets <- search_tweets(word, n = 18000, include_rts = FALSE)
-  tweets <- tweets %>% 
-    mutate(termo = word) %>% 
-    mutate(week = epiweek(created_at))
+get_pressao <- function(tweets) {
   pressao <- tweets %>%
     group_by(termo, week) %>%
     summarise(tweets = n(),
@@ -20,3 +10,33 @@ get_tweets_pls <- function(words_df) {
               favs = sum(favorite_count))
 }
 
+get_tweets_pls <- function(words_df) {
+  tweets_apelidos <- purrr::map_df(words_df$apelido, ~ .get_tweets(.x))
+  tweets_nome_formal <- purrr::map_df(words_df$nome_formal, ~ .get_tweets(.x))
+  tweets <- bind_rows(tweets_apelidos, tweets_nome_formal)
+}
+
+.get_tweets <- function(word) {
+  cat(paste0("\n", "Baixando tweets com termo: '", word, "'...", "\n"))
+  tweets <- search_tweets(word, n = 250000, retryonratelimit = TRUE, include_rts = FALSE)
+  if (check_dataframe(tweets)) {
+    tweets <- tweets %>% 
+      mutate(termo = word) %>% 
+      mutate(week = epiweek(created_at))
+  }
+  tweets
+
+}
+
+#' @title Verifica dataframe
+#' @description Verifica se um determinado daframe dado é nulo ou vazio.
+#' @param df Dataframe a ser verificado
+#' @return Dataframe vazio.
+#' @export
+check_dataframe <- function(df) {
+  if ((is.null(df) || (nrow(df) == 0))) {
+    warning("Dataframe de entrada deve ser não-nulo e não-vazio.")
+    return(FALSE)
+  }
+  return(TRUE)
+}
