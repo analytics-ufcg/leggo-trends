a540fa000ecba5773ff0c3ab40fd141c15bf9f71
Matheus Leal <matheus.leal@ccc.ufcg.edu.br>
2019-08-08 16:35:04 -0300

    Adicionando script que atualiza tweets e trends

--- R/twitter.R
+++ R/twitter.R
@@ -1,10 +1,3 @@
-source("http://news.mrdwab.com/install_github.R")
-install_github("mrdwab/SOfun")
-library(SOfun)
-library(rtweet)
-library(lubridate)
-library(tidyverse)
-
 #' @title Gera estatísticas dos termos no twitter
 #' @description Cria um dataframe com 'desempenho' dos termos no twitter por semana
 #' @param tweets Dataframe com tweets
@@ -12,10 +5,10 @@ library(tidyverse)
 #' @export
 generate_twitter_trends <- function(tweets) {
   pressao <- tweets %>%
-    group_by(termo, week) %>%
-    summarise(tweets = n(),
-              retweets = sum(retweet_count),
-              favs = sum(favorite_count))
+    dplyr::group_by(termo, week) %>%
+    dplyr::summarise(tweets = n(),
+              retweets = sum(as.integer(retweet_count)),
+              favs = sum(as.integer((favorite_count))))
 }
 
 #' @title Retorna tweets recentes
@@ -26,13 +19,13 @@ generate_twitter_trends <- function(tweets) {
 get_tweets_pls <- function(words_df) {
   tweets_apelidos <- purrr::map_df(words_df$apelido, ~ .get_tweets(.x))
   tweets_nome_formal <- purrr::map_df(words_df$nome_formal, ~ .get_tweets(.x))
-  tweets <- bind_rows(tweets_apelidos, tweets_nome_formal)
+  tweets <- dplyr::bind_rows(tweets_apelidos, tweets_nome_formal)
   cols <- sapply(tweets, class)
   cols_type_list <- cols %in% 'list'
   names_cols <- cols[cols_type_list]
   columns <- names(names_cols)
-  tweets_unlist <- col_flatten(tweets, cols = columns)
-  tweets_unlist <- tweets_unlist %>% select(-columns)
+  tweets_unlist <- SOfun::col_flatten(tweets, cols = columns)
+  tweets_unlist <- tweets_unlist %>% dplyr::select(-columns)
 }
 
 #' @title Retorna tweets recentes
@@ -41,11 +34,11 @@ get_tweets_pls <- function(words_df) {
 #' @return Dataframe com tweets
 .get_tweets <- function(word) {
   cat(paste0("\n", "Baixando tweets com termo: '", word, "'...", "\n"))
-  tweets <- search_tweets(word, n = 250000, retryonratelimit = TRUE, include_rts = FALSE)
-  if (check_dataframe(tweets)) {
+  tweets <- rtweet::search_tweets(word, n = 250000, retryonratelimit = TRUE, include_rts = FALSE)
+  if (leggoTrends::check_dataframe(tweets)) {
     tweets <- tweets %>%
-      mutate(termo = word) %>%
-      mutate(week = epiweek(created_at))
+      dplyr::mutate(termo = word) %>%
+      dplyr::mutate(week = lubridate::epiweek(created_at))
   }
   tweets
 
